c9067b7793d1da60123e4d0b09cf8d31029e77af
Erik Wong <k4orta@gmail.com>
2013-08-02 22:44:34 -0700

    turn off the lights

--- app.py
+++ app.py
@@ -10,10 +10,12 @@ GPIO.setup(RED_LED, GPIO.OUT)
 red_state = False
 green_state = True
 
-while True:
-  red_state = not red_state
-  green_state = not green_state
-  GPIO.output(GREEN_LED, green_state)
-  GPIO.output(RED_LED, red_state)
-  time.sleep(1)
+# while True:
+#   red_state = not red_state
+#   green_state = not green_state
+#   GPIO.output(GREEN_LED, green_state)
+#   GPIO.output(RED_LED, red_state)
+#   time.sleep(1)
 
+GPIO.output(GREEN_LED, False)
+GPIO.output(RED_LED, False)
